32b3b5d01d2fbdad0aedfb5428c20c5a8ed664c7
shirocola <kitpivatpiromgit@gmail.com>
2024-11-18 22:50:21 +0700

    write first test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Welcome to the Developer Portal!!"', () => {
+      expect(appController.getHello()).toBe('Welcome to the Developer Portal!');
     });
   });
 });
