78a9d234ab8252843e845398c1f6710cc3392ee8
shirocola <kitpivatpiromgit@gmail.com>
2024-11-19 19:47:22 +0700

    add cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,13 +5,21 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Enable CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // Specify the origin(s) allowed to access your API
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+    credentials: true, // Allow credentials such as cookies
+  });
+
+  // Configure Swagger
   const config = new DocumentBuilder()
     .setTitle('Developer Portal API')
-    .setDescription('API documentaion for the Developer Portal')
+    .setDescription('API documentation for the Developer Portal')
     .setVersion('1.0')
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup('api-docs', app, document)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
 
   await app.listen(4000);
 }
